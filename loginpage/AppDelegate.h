929cf23f498d3f65fd195aeeab0fa18dc33da365
Cam <developer@developers-MacBook-Pro.local>
2014-09-10 09:54:34 +0800

    Initial Commit

---
+++ loginpage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  loginpage
+//
+//  Created by Cam on 14-9-10.
+//  Copyright (c) 2014年 Cam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ loginpage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  loginpage
+//
+//  Created by Cam on 14-9-10.
+//  Copyright (c) 2014年 Cam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
